487138357bc676ce8c6b3167cff02fa042ce0b70
Ganeshpr123 <77228758+Ganeshpr123@users.noreply.github.com>
2022-10-14 23:26:08 +0530

    VERSION 1.0

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include("./maininclude/header.php");
     <div class="vid-overly"></div>
   </div>
   <div class="vid-content">
-    <h1 class="my-content">Welcome to Ganesh </h1>
+    <h1 class="my-content">Welcome to Ganesh 1.0 </h1>
     <small class="my-content">Learn and Earn TO</small><br><br>
     <a href="#" class="btn btn-danger" data-toggle="modal" data-target="#stuRegModelCenter">Get Started</a>
        
